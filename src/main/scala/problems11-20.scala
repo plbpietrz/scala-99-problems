7b06463938b612b8bee8b3ec087c7b6b36881bca
CreateThreadEx <bartlomiej.pietrzyk@live.com>
2016-05-12 00:03:31 +0200

    Problem 12

--- src/main/scala/problems11-20.scala
+++ src/main/scala/problems11-20.scala
@@ -6,6 +6,13 @@ object Problems11to20 {
   }
 
   def decode[T](xs: List[(Int, T)]): List[T] = {
+    def aux[T](n: Int, a: T, as: List[T]): List[T] = {
+      if (n == 0) as else aux(n-1, a, a::as)
+    }
+    xs.foldLeft(List[T]())((a, x) => aux(x._1, x._2, a)).reverse
+  }
+  
+  def encodeDirect[T](xs: List[T]): List[(Int,T)] = {
     Nil
   }
 
